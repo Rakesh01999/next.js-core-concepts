fb3da528a7a0990459bc3c6fe66793d2b1c7a2d4
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-17 16:35:40 +0600

    Home page modified

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
+import ProductCard from "@/components/Products/ProductCard";
 
-const HomePage = async() => {
+const HomePage = async () => {
   const res = await fetch("http://localhost:5000/products");
   const products = await res.json();
   // console.log(products)
   return (
     <div className="text-center text-4xl my-5">
       <h1>Data Fetching, Caching and Revalidating</h1>
+      <div className="grid grid-cols-3 gap-8 w-[90%] mx-auto mt-10">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
