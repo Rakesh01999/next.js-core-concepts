3c78418c5fa259fb7a5b050a1756f8b7b9acf19f
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-17 22:41:34 +0600

    Loading Page created

---
+++ src/app/products/loading.js
@@ -0,0 +1,11 @@
+const LoadingPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-center text-blue-600 font-bold my-auto">
+        Loading . . .
+      </h1>
+    </div>
+  );
+};
+
+export default LoadingPage;
