3f26cd7c330e6bdfa06bd4f0d3a477acd30dc13b
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-18 01:06:05 +0600

    Navbar UI modified

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,25 +1,47 @@
 "use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathName = usePathname();
+  // console.log(pathName);
+
   return (
-    <nav className="bg-blue-600 text-white py-4 shadow-md">
+    <nav className="bg-gray-500 text-white py-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="text-2xl font-bold">
           Next Nav
         </Link>
         <div className="space-x-6">
-          <Link href="/about" className="hover:text-gray-200">
+          <Link
+            href="/about"
+            className={
+              pathName === "/about"
+                ? " text-purple-300 shadow-md shadow-purple-400 font-bold cursor-pointer underline hover:text-gray-200 hover:underline"
+                : "hover:text-gray-200"
+            }
+          >
             About Us
           </Link>
-          <Link href="/gallery" className="hover:text-gray-200">
+          <Link
+            href="/gallery"
+            className={
+              pathName === "/gallery"
+                ? " text-purple-300 shadow-md shadow-purple-400 font-bold cursor-pointer underline hover:text-gray-200 hover:underline"
+                : "hover:text-gray-200"
+            }
+          >
             Gallery
           </Link>
-          <Link href="/products" className="hover:text-gray-200">
+          <Link
+            href="/products"
+            className={
+              pathName === "/products"
+                ? " text-purple-300 shadow-md shadow-purple-400 font-bold cursor-pointer underline hover:text-gray-200 hover:underline"
+                : "hover:text-gray-200"
+            }
+          >
             Products
-          </Link>
-          <Link href="/register" className="hover:text-gray-200">
-            Register
           </Link>
         </div>
       </div>
