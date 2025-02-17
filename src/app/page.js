74291a8479416e1b17bd7e3a67e400d087f5eb7d
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-17 22:16:04 +0600

    new data added and 3 of them are shown in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,19 @@
 import ProductCard from "@/components/Products/ProductCard";
 
 const HomePage = async () => {
-  const res = await fetch("http://localhost:5000/products");
+  const res = await fetch("http://localhost:5000/products", {
+    // cache: "force-cache",
+    next: {
+      revalidate: 5,
+    },
+  });
   const products = await res.json();
   // console.log(products)
   return (
     <div className="text-center text-4xl my-5">
       <h1>Data Fetching, Caching and Revalidating</h1>
       <div className="grid grid-cols-3 gap-8 w-[90%] mx-auto mt-10">
-        {products.map((product) => (
+        {products.slice(0,3).map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
