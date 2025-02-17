18f9dd2715a8c50d4e4f35650b997ac6b21c306c
Rakesh Biswas <rbiswas01999@gmail.com>
2025-02-17 14:26:06 +0600

    absolute-importation done in gallery-page

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,8 +1,11 @@
 import Image from "next/image";
-import nextImage from "../../assets/next image.png"
+// import nextImage from "../../assets/next image.png";
+import nextImage from "@/assets/next image.png";
+import Navbar from "@/components/Navbar";
 const GalleryPage = () => {
   return (
-    <div>
+    <div className="text-center">
+      <Navbar />
       <h1 className="text-2xl text-center">Regular Image Tag</h1>
       <img
         src="https://nextjs.org/api/docs-og?title=Optimizing%20Images"
